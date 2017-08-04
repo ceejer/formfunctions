862ede6fcc6eeccc0ef5f21aa1d23b5fef16b07d
Chris <cjaynes82@gmail.com>
2017-08-04 19:32:40 -0400

    Update combobox.js

--- combobox.js
+++ combobox.js
@@ -30,50 +30,37 @@
  * OTHER DEALINGS IN THE SOFTWARE.
  *
  */
-
 (function( $, undefined ) {
-
    $.widget( "ui.combobox", {
-
       version: "@VERSION",
-
       widgetEventPrefix: "combobox",
       keepinvalid:false,
       uiCombo: null,
       uiInput: null,
       _wasOpen: false,
-     
-
       _create: function() {
-
          var self = this,
              select = this.element.hide(),
              input, wrapper;
-
          input = this.uiInput =
                   $( "<input />" )
                       .insertAfter(select)
                       .addClass("ui-widget ui-widget-content ui-corner-left ui-combobox-input")
                       .val( select.children(':selected').text() )
-                      .attr('tabindex', select.attr( 'tabindex'));
-
+                      .attr('tabindex', select.attr('tabindex'));
+         select.addClass('comboboxsource');
          wrapper = this.uiCombo =
             input.wrap( '<span>' )
                .parent()
                .addClass( 'ui-combobox' )
                .insertAfter( select );
-
          input
           .autocomplete({
-
              delay: 0,
              minLength: 0,
-
              appendTo: wrapper,
              source: $.proxy( this, "_linkSelectList" )
-
           });
-
          $( "<button>" )
             .attr( "tabIndex", -1 )
             .attr( "type", "button" )
@@ -86,8 +73,6 @@
             })
             .removeClass( "ui-corner-all ui-button-icon-only" )
             .addClass( "ui-corner-right ui-button-icon ui-combobox-button" );
-
-
          // Our items have HTML tags.  The default rendering uses text()
          // to set the content of the <a> tag.  We need html().
          input.data( "ui-autocomplete" )._renderItem = function( ul, item ) {
@@ -98,21 +83,14 @@
              retli.attr("title",item.option.title);
            }
            return retli;
-
             };
-
          this._on( this._events );
-
       },
-
-
       _linkSelectList: function( request, response ) {
-
          var matcher = new RegExp( $.ui.autocomplete.escapeRegex(request.term), 'i' );
          response( this.element.children('option').map(function() {
                   var text = $( this ).text();
                   var optvl = $(this).val();
-
                   if ( this.value && ( !request.term || matcher.test(text) || matcher.test(optvl) ) ) {
                      var optionData = {
                          label: text,
@@ -132,15 +110,11 @@
               })
            );
       },
-
       _events: {
-
          "autocompletechange input" : function(event, ui) {
-
             var $el = $(event.currentTarget);
             var changedOption = ui.item ? ui.item.option : null;
             if ( !ui.item ) {
-
                var matcher = new RegExp( "^" + $.ui.autocomplete.escapeRegex( $el.val() ) + "$", "i" ),
                valid = false,
                matchContains = null,
@@ -151,8 +125,6 @@
                if (this.options.autofillsinglematch) {
                   matchContains = new RegExp($.ui.autocomplete.escapeRegex($el.val()), "i");
                }
-
-
                this.element.children( "option" ).each(function() {
                      var t = $(this).text();
                      var v = $(this).val();
@@ -179,7 +151,6 @@
                         }
                      }
                   });
-
                 if ( !valid ) {
                    // autofill option:  if there is just one match, then select the matched option
                    if (iContains == 1) {
@@ -196,73 +167,51 @@
                      newopt.text($el.val());
                      newopt.prop("selected",true);
 				   this.element.append(newopt);
-
-                           
                    } else {
-
                       // remove invalid value, as it didn't match anything
                       $el.val( '' );
-
                       // Internally, term must change before another search is performed
                       // if the same search is performed again, the menu won't be shown
                       // because the value didn't actually change via a keyboard event
                       $el.data( 'ui-autocomplete' ).term = '';
-
                       this.element.prop('selectedIndex', -1);
                    }
                 }
             }
-
             this._trigger( "change", event, {
                   item: changedOption
                 });
-
          },
-
          "autocompleteselect input": function( event, ui ) {
-
             ui.item.option.selected = true;
             this._trigger( "select", event, {
                   item: ui.item.option
                });
-
          },
-
          "autocompleteopen input": function ( event, ui ) {
-
             this.uiCombo.children('.ui-autocomplete')
                .outerWidth(this.uiCombo.outerWidth(true));
          },
-
          "mousedown .ui-combobox-button" : function ( event ) {
             this._wasOpen = this.uiInput.autocomplete("widget").is(":visible");
          },
-
          "click .ui-combobox-button" : function( event ) {
-
             this.uiInput.focus();
-
             // close if already visible
             if (this._wasOpen)
                return;
-
             // pass empty string as value to search for, displaying all results
             this.uiInput.autocomplete("search", "");
-
          }
-
       },
-
       value: function ( newVal ) {
          var select = this.element,
              valid = false,
              selected;
-
          if ( !arguments.length ) {
             selected = select.children( ":selected" );
             return selected.length > 0 ? selected.val() : null;
          }
-
          select.prop('selectedIndex', -1);
          select.children('option').each(function() {
                if ( this.value == newVal ) {
@@ -270,7 +219,6 @@
                   return false;
                }
             });
-
          if ( valid ) {
             this.uiInput.val(select.children(':selected').text());
          } else if(keepinvalid) {
@@ -279,28 +227,20 @@
             this.uiInput.val( "" );
             this.element.prop('selectedIndex', -1);
          }
-
       },
-
       _destroy: function () {
          this.element.show();
          this.uiCombo.replaceWith( this.element );
       },
-
       widget: function () {
          return this.uiCombo;
       },
-
       _getCreateEventData: function() {
-
          return {
             select: this.element,
             combo: this.uiCombo,
             input: this.uiInput
          };
       }
-
     });
-
-
 }(jQuery));
